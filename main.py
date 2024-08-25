3b15489ce336965ae7efa240b66086ebb5e9f72c
lucjalbert <lucjalbert01@gmail.com>
2024-08-24 22:28:16 -0400

    add player and player movement

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
-        screen_surface = pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for object in updatable:
+            object.update(dt)
+    
+        screen.fill((0, 0, 0))
+
+        for object in drawable:
+            object.draw(screen)
+            
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
